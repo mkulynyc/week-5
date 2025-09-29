789762b870634555e545d86553412a40f94f51c6
Matthew Kulynych <mkulynyc@iu.edu>
2025-09-29 18:57:49 +0000

    moved functions around

--- apputil.py
+++ apputil.py
@@ -6,19 +6,18 @@ df = pd.read_csv('https://raw.githubusercontent.com/leontoddjohnson/datasets/mai
 
 df.rename(columns={'Pclass': 'pclass', 'Sex': 'sex'}, inplace=True)
 
-df['age_group'] = pd.cut(
-    df['Age'],
-    bins=[0, 12, 19, 59, 100],
-    labels=['Child', 'Teenager', 'Adult', 'Senior']
-)
-
-df['age_group'] = df['age_group'].astype(pd.CategoricalDtype(
-    categories=['Child', 'Teenager', 'Adult', 'Senior'],
-    ordered=True
-))
-
 ### Exercise 1 ###
 def survival_demographics():
+    df['age_group'] = pd.cut(
+        df['Age'],
+        bins=[0, 12, 19, 59, 100],
+        labels=['Child', 'Teenager', 'Adult', 'Senior']
+    )
+
+    df['age_group'] = df['age_group'].astype(pd.CategoricalDtype(
+        categories=['Child', 'Teenager', 'Adult', 'Senior'],
+        ordered=True
+    ))
     all_combinations = pd.MultiIndex.from_product(
         [df['pclass'].unique(), df['sex'].unique(), df['age_group'].cat.categories],
         names=['pclass', 'sex', 'age_group']
